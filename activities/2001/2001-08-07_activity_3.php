fd98d482a54ad516468915b5c67d9274dcef277f
Tamicktom <rickpokemon38@gmail.com>
2001-08-07 13:23:41 -0300

    ✨ PHP: Atividade #4 em 07/08/2001

---
+++ activities/2001/2001-08-07_activity_3.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Implementando APIs 🌐
+ * Data: 07/08/2001
+ * Commit #3
+ */
+
+class DailyActivity {
+
+    private $activities = [
+        "Desenvolvendo APIs Laravel",
+        "Corrigindo bugs no backend",
+        "Otimizando queries MySQL",
+        "Implementando autenticação JWT",
+        "Escrevendo testes PHPUnit",
+        "Refatorando controllers MVC"
+    ];
+
+    private $currentActivity = "Implementando APIs 🌐";
+    private $date = "07/08/2001";
+
+    public function executeActivity() {
+        echo "Data: " . $this->date . "\n";
+        echo "Atividade: " . $this->currentActivity . "\n";
+    }
+}
+
+$activity = new DailyActivity();
+$activity->executeActivity();
+
+?>
