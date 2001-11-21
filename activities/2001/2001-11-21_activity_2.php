e691dc5416118d0270b9beb133a68eb2246020f4
Tamicktom <rickpokemon38@gmail.com>
2001-11-21 11:25:28 -0200

    ✨ PHP: Atividade #3 em 21/11/2001

---
+++ activities/2001/2001-11-21_activity_2.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Resolvendo bugs 🐛
+ * Data: 21/11/2001
+ * Commit #2
+ */
+
+class DailyActivity {
+
+    private $activities = [
+        "Desenvolvendo APIs Laravel",
+        "Corrigindo bugs no backend",
+        "Otimizando queries MySQL",
+        "Implementando autenticação JWT",
+        "Escrevendo testes PHPUnit",
+        "Refatorando controllers MVC"
+    ];
+
+    private $currentActivity = "Resolvendo bugs 🐛";
+    private $date = "21/11/2001";
+
+    public function executeActivity() {
+        echo "Data: " . $this->date . "\n";
+        echo "Atividade: " . $this->currentActivity . "\n";
+    }
+}
+
+$activity = new DailyActivity();
+$activity->executeActivity();
+
+?>
