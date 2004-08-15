f000a03e590308a026a16628b3992294b92c61db
Tamicktom <rickpokemon38@gmail.com>
2004-08-15 18:14:40 -0300

    ✨ PHP: Atividade #4 em 15/08/2004

---
+++ activities/2004/2004-08-15_activity_3.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Fazendo code review 👀
+ * Data: 15/08/2004
+ * Commit #3
+ */
+
+class DailyActivity {
+
+    private $activities = [
+        "Desenvolvendo APIs Laravel",
+        "Corrigindo bugs no backend",
+        "Otimizando queries MySQL",
+        "Implementando autenticação JWT",
+        "Escrevendo testes PHPUnit",
+        "Refatorando controllers MVC"
+    ];
+
+    private $currentActivity = "Fazendo code review 👀";
+    private $date = "15/08/2004";
+
+    public function executeActivity() {
+        echo "Data: " . $this->date . "\n";
+        echo "Atividade: " . $this->currentActivity . "\n";
+    }
+}
+
+$activity = new DailyActivity();
+$activity->executeActivity();
+
+?>
